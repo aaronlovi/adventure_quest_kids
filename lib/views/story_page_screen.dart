23850fab923ce415ca6af548239c389bcabdead2
Aaron Lovi <aaron.lovi@4poker.com>
2023-11-26 15:51:44 -0500

    Added animated glow to each word

--- lib/views/story_page_screen.dart
+++ lib/views/story_page_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:adventure_quest_kids/model/story.dart';
 import 'package:adventure_quest_kids/model/story_choice.dart';
 import 'package:adventure_quest_kids/model/story_page.dart';
@@ -19,6 +21,8 @@ class StoryPageScreen extends StatefulWidget {
 
 class StoryPageScreenState extends State<StoryPageScreen> {
   late AudioPlayer _player;
+  final ValueNotifier<int> _currentWordIndex = ValueNotifier<int>(-1);
+  Timer? _timer;
 
   String get imagePath =>
       '${widget.story.imagesFolder}/${widget.storyPage.imageFileName}';
@@ -36,68 +40,150 @@ class StoryPageScreenState extends State<StoryPageScreen> {
 
   @override
   void dispose() {
+    _currentWordIndex.dispose();
+    _timer?.cancel();
     super.dispose();
     // _player.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> choicesWidgets = _getChoiceWidgets(context);
+    var bottomHalfWidgets = <Widget>[];
+    _addStoryTextWidgets(bottomHalfWidgets);
+    _addStoryChoiceWidgets(context, bottomHalfWidgets);
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.story.title),
-          actions: [
-            IconButton(
-              tooltip: 'Start of story',
-              onPressed: () {
-                Navigator.popUntil(context, ModalRoute.withName('front-page'));
-              },
-              icon: const Icon(Icons.home),
-            ),
-            IconButton(
-              tooltip: 'Story list',
-              onPressed: () {
-                Navigator.popUntil(context, (route) => route.isFirst);
-              },
-              icon: const Icon(Icons.list),
-            ),
-          ],
-        ),
-        body: Column(
-          children: [
-            // Top half: Container for Image
-            Container(
-                height: MediaQuery.of(context).size.height * 0.4,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    fit: BoxFit.contain,
-                    image: AssetImage(imagePath),
-                  ),
-                )),
-            // Bottom half: Text and Choices
-            Container(
+      appBar: AppBar(
+        title: Text(widget.story.title),
+        actions: [
+          IconButton(
+            tooltip: 'Start of story',
+            onPressed: () {
+              Navigator.popUntil(context, ModalRoute.withName('front-page'));
+            },
+            icon: const Icon(Icons.home),
+          ),
+          IconButton(
+            tooltip: 'Story list',
+            onPressed: () {
+              Navigator.popUntil(context, (route) => route.isFirst);
+            },
+            icon: const Icon(Icons.list),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          // Top half: Container for Image
+          Container(
               height: MediaQuery.of(context).size.height * 0.4,
-              padding: const EdgeInsets.all(16),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: choicesWidgets,
-              ),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.contain,
+                  image: AssetImage(imagePath),
+                ),
+              )),
+          // Bottom half: Text and Choices
+          Container(
+            height: MediaQuery.of(context).size.height * 0.4,
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: bottomHalfWidgets,
             ),
-          ],
-        ));
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: _startAnimation,
+          tooltip: 'Start Animation',
+          child: const Icon(Icons.play_arrow)),
+    );
   }
 
-  List<Widget> _getChoiceWidgets(BuildContext context) {
-    var widgets = <Widget>[];
+  void _addStoryTextWidgets(List<Widget> widgets) {
+    List<String> words = widget.storyPage.text.split(' ');
 
-    widgets.add(Center(
-      child: Text(
-        widget.storyPage.text,
-        textAlign: TextAlign.center,
-        style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+    AnimatedBuilder pageTextWidget = _buildAnimatedStoryText(words);
+
+    widgets.add(Center(child: pageTextWidget));
+  }
+
+  AnimatedBuilder _buildAnimatedStoryText(List<String> words) {
+    return AnimatedBuilder(
+      animation: _currentWordIndex,
+      builder: (context, _) {
+        return RichText(
+          textAlign: TextAlign.center,
+          text: TextSpan(
+            style: const TextStyle(
+                fontSize: 16, fontWeight: FontWeight.bold, color: Colors.black),
+            children: words
+                .expand((word) =>
+                    _buildStoryTextWordSpan(word, words.indexOf(word)))
+                .toList(),
+          ),
+        );
+      },
+    );
+  }
+
+  List<InlineSpan> _buildStoryTextWordSpan(String word, int index) {
+    return [
+      WidgetSpan(child: _buildStoryTextStack(word, index)),
+      const TextSpan(text: ' '),
+    ];
+  }
+
+  Stack _buildStoryTextStack(String word, int index) {
+    return Stack(
+      children: [
+        // Base story page text
+        Text(word,
+            style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.black)),
+
+        // Story page text that glows and underlines when animated
+        _buildStoryTextAnimatedOpacity(word, index),
+      ],
+    );
+  }
+
+  AnimatedOpacity _buildStoryTextAnimatedOpacity(String word, int index) {
+    return AnimatedOpacity(
+      opacity: index == _currentWordIndex.value && _currentWordIndex.value != -1
+          ? 1.0
+          : 0.0,
+      duration: const Duration(milliseconds: 500),
+      child: _buildStoryTextContainer(word),
+    );
+  }
+
+  Container _buildStoryTextContainer(String word) {
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.yellow.withOpacity(0.6),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: const Offset(0, 3),
+          ),
+        ],
       ),
-    ));
+      child: Text(word,
+          style: const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+              decoration: TextDecoration.underline)),
+    );
+  }
+
+  List<Widget> _addStoryChoiceWidgets(
+      BuildContext context, List<Widget> widgets) {
     widgets.add(const SizedBox(height: 16));
 
     for (String choiceName in widget.storyPage.choices.keys) {
@@ -180,5 +266,21 @@ class StoryPageScreenState extends State<StoryPageScreen> {
         print('Error playing sound: $e');
       }
     }
+  }
+
+  void _startAnimation() {
+    List<String> words = widget.storyPage.text.split(' ');
+
+    _timer?.cancel();
+    _currentWordIndex.value = 0;
+
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (_currentWordIndex.value < words.length - 1) {
+        _currentWordIndex.value++;
+      } else {
+        timer.cancel();
+        _currentWordIndex.value = -1;
+      }
+    });
   }
 }
