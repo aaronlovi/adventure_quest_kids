8349937f4d55c7facb49d806fba8da763a2d954c
Aaron Lovi <aaronlovi@gmail.com>
2025-05-21 18:35:35 -0400

    Update build configuration to use Java 17 and add signing configuration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,8 +1,15 @@
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+}
+
+// Load key.properties
+val keystoreProps = Properties().apply {
+    load(rootProject.file("key.properties").inputStream())
 }
 
 android {
@@ -11,12 +18,12 @@ android {
     ndkVersion = "27.0.12077973"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
@@ -31,11 +38,20 @@ android {
         manifestPlaceholders["google_ads_app_id"] = project.findProperty("google_ads_app_id") ?: ""
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file(keystoreProps["storeFile"] as String)
+            storePassword = keystoreProps["storePassword"] as String
+            keyAlias = keystoreProps["keyAlias"] as String
+            keyPassword = keystoreProps["keyPassword"] as String
+        }
+    }
+
     buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = false      // change if you use R8/ProGuard
+            isShrinkResources = false
         }
     }
 }
